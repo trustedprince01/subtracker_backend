ce9d5fbbecbef6a8da2d29c9a2bc24aeb646a8cf
trustedprince01 <chibuzorprince68@gmail.com>
2025-05-10 05:58:11 -0700

    RESOLVED URL

--- subtracker_api/settings.py
+++ subtracker_api/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://sub-tracker.up.railway.app/',  # This will allow all Railway subdomains
+    'sub-tracker.up.railway.app',
 ]
 
 
