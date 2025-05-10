ff961a94b53060722e06b834e33f48ee5fcd44a1
trustedprince01 <chibuzorprince68@gmail.com>
2025-05-10 06:41:30 -0700

    RESOLVED URL

--- subtracker_api/settings.py
+++ subtracker_api/settings.py
@@ -233,4 +233,4 @@ DEFAULT_FROM_EMAIL = 'noreply@subtracker.com'
 # Tell dj-rest-auth to use JWT instead of token auth
 REST_USE_JWT = True
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
